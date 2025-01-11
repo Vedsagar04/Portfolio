ff4e33647063418244efe534b2e985be61a1cb9f
Vedprakash Verma <97621768+Vedsagar04@users.noreply.github.com>
2025-01-11 20:40:17 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div id="home" className="hero">
             <img src={image} alt="profile" />
             <h1><span>Vedprakash Verma</span> <br /></h1><h2>Frontend Developer</h2>
-            <p>"Creative Frontend Developer proficient in HTML, CSS, and JavaScript, creating responsive, user-friendly interfaces that enhance user experience and engagement."</p>
+            <p>Creative Frontend Developer proficient in HTML, CSS, and JavaScript, creating responsive, user-friendly interfaces that enhance user experience and engagement.</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href="#contact">Connect With Me</AnchorLink></div>
                 <div className="hero-resume">My Resume</div>
