a732808272d10afc0898473e8e3758072ecb97f9
Vedprakash Verma <97621768+Vedsagar04@users.noreply.github.com>
2025-01-11 20:35:07 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div id="home" className="hero">
             <img src={image} alt="profile" />
             <h1><span>Vedprakash Verma</span> <br /></h1><h2>Frontend Developer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officiis itaque asperiores quo eaque perferendis, quia tenetur. Debitis dolorum impedit eligendi dicta excepturi qui possimus natus autem laborum omnis inventore aliquid tenetur, molestiae cum, ex enim ratione quos unde quas adipisci voluptatibus voluptate provident temporibus quis. Quod deleniti labore maxime?</p>
+            <p>"Creative Frontend Developer proficient in HTML, CSS, and JavaScript, creating responsive, user-friendly interfaces that enhance user experience and engagement."</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href="#contact">Connect With Me</AnchorLink></div>
                 <div className="hero-resume">My Resume</div>
